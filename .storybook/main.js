7c3b3aed4aee00fe81c4fea9ae3ca6e1c55e8df5
Oustaa <otailaba98@gmail.com>
2024-09-20 22:53:21 +0100

    test

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../packages/components/src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+};
+
