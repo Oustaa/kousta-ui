c57ab66fef1d2cd4888f0087d06f45da7e630b33
Oustaa <otailaba98@gmail.com>
2024-09-25 20:33:45 +0100

    chore(fixes): some minor changes

--- packages/table/src/Table.tsx
+++ packages/table/src/Table.tsx
@@ -6,28 +6,28 @@ type dataInterface = {
   name: string;
 };
 
-// eslint-skip-next-line
-const Table = ({ data }: { data: dataInterface[] }) => (
-  <table className="table">
-    <thead>
-      <tr className="header">
-        {Object.keys(data[0]).map((key) => (
-          <th key={key}>{key}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map((row, rowIndex) => (
-        <tr key={rowIndex}>
-          {Object.values(row).map((value, colIndex) => (
-            <td key={colIndex} className="cell">
-              {value as string}
-            </td>
+const Table = ({ data }: { data: dataInterface[] }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr className="header">
+          {Object.keys(data[0]).map((key) => (
+            <th key={key}>{key}</th>
           ))}
         </tr>
-      ))}
-    </tbody>
-  </table>
-);
-
+      </thead>
+      <tbody>
+        {data.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {Object.values(row).map((value, colIndex) => (
+              <td key={colIndex} className="cell">
+                {value as string}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 export default Table;
